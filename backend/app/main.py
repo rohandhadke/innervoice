a826fef917466ac12e3f7363f318c0669a2cd076
rohandhadke <rohandhadke7620@gmail.com>
2026-07-14 00:38:18 +0530

    feat: allowed origin vercel frontend app

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,8 +24,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://innervoice.vercel.app" 
-    ], 
+        "https://innervoice.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
